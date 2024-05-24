ecbd964a11e1ca85285b0ad705c090de3104314c
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-05-24 08:06:02 +0300

    refactor: change return view in '/' route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    \Illuminate\Support\Facades\Auth::attempt();
-    return view('welcome');
-});
+    return view('main');
+})->name('home');
+
