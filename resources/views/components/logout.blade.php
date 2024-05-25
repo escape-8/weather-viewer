5b147667b108cb53cb25e923d3565df9393d5f2c
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-05-25 11:58:12 +0300

    refactor: remove container for buttons in logout.blade.php

--- resources/views/components/logout.blade.php
+++ resources/views/components/logout.blade.php
@@ -1,7 +1,5 @@
-<div class="d-flex gap-4">
-    <div class="align-self-center text-light">{{ Auth::user()->email }}</div>
-    <form action="{{ route('user.logout') }}" method="POST">
-        @csrf
-        <button type="submit" class="btn btn-light">Logout</button>
-    </form>
-</div>
+<div class="align-self-center text-light">{{ Auth::user()->email }}</div>
+<form action="{{ route('user.logout') }}" method="POST">
+    @csrf
+    <button type="submit" class="btn btn-light">Logout</button>
+</form>
