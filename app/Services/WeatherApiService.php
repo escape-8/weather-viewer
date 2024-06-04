6edfea71b1d8aa901cbd24a3b1c018b7608711c3
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-06-04 16:40:02 +0300

    feat: implement WeatherApiService

---
+++ app/Services/WeatherApiService.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class WeatherApiService
+{
+    private string $apiKey;
+    private string $geoLink;
+    private string $weatherLink;
+    private string $forecastLink;
+    private string $unitsOfMeasurement = 'metric';
+    private int $numberOfTimestamps = 8;
+    private int $limitReturnLocation = 4;
+
+    public function __construct()
+    {
+        $this->apiKey = env('WEATHER_API_KEY');
+        $this->geoLink = 'https://api.openweathermap.org/geo/1.0/direct';
+        $this->weatherLink = 'https://api.openweathermap.org/data/2.5/weather';
+        $this->forecastLink = 'https://api.openweathermap.org/data/2.5/forecast';
+    }
+
+
+    public function getLocationByCityName(?string $cityName): array|object
+    {
+        $response = Http::withQueryParameters([
+            'q' => $cityName,
+            'limit' => $this->limitReturnLocation,
+            'appid' => $this->apiKey,
+        ])->get($this->geoLink);
+
+        $statusCode = $response->status();
+
+        return match ($statusCode) {
+            200 => $response->object(),
+            400 => throw new BadRequestException($response->object()->message, $statusCode),
+            default => throw new HttpException($statusCode, $response->object()->message),
+        };
+    }
+
+    public function getWeatherByCoordinates(string $latitude, string $longitude): object
+    {
+        $response = Http::acceptJson()->get($this->weatherLink,
+            [
+            'lat' => $latitude,
+            'lon' => $longitude,
+            'appid' => $this->apiKey,
+            'units' => $this->unitsOfMeasurement,
+        ]);
+        return $response->object();
+    }
+
+    public function getForecastWeatherByCoordinates(string $latitude, string $longitude): object
+    {
+        $response = Http::withQueryParameters([
+            'lat' => $latitude,
+            'lon' => $longitude,
+            'appid' => $this->apiKey,
+            'units' => $this->unitsOfMeasurement,
+            'cnt' => $this->numberOfTimestamps,
+        ])->get($this->forecastLink);
+        return $response->object();
+    }
+}
