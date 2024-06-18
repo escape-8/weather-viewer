bfff15fb3d6dc634e62b805c25ebd260dfcff366
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-06-18 10:47:49 +0300

    fix: fix when location already add in weather track message not view

--- resources/views/page/search-result.blade.php
+++ resources/views/page/search-result.blade.php
@@ -6,6 +6,14 @@
     </div>
 @endsection
 
+@section('flash')
+    @if (session('status'))
+        <div class="container alert alert-{{ session('alert') ?? 'success' }} mt-5 text-center">
+            {{ session('status') }}
+        </div>
+    @endif
+@endsection
+
 @section('content')
     <div class="my-5 d-flex flex-wrap align-items-start justify-content-center gap-5 h-50 w-100">
         @isset ($searchError)
